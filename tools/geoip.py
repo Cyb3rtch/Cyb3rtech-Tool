7a3398a8f05612e703ff55811f30df12b695e6c3
Cyb3rtch <174739196+Cyb3rtch@users.noreply.github.com>
2024-08-18 03:23:40 +0200

    Update geoip.py

--- tools/geoip.py
+++ tools/geoip.py
@@ -5,6 +5,25 @@ import concurrent.futures
 import platform
 import requests
 
+menu = """
+      ██▓ ██▓███      ██▓     ▒█████   ▒█████   ██ ▄█▀ █    ██  ██▓███
+      ▓██▒▓██░  ██▒   ▓██▒    ▒██▒  ██▒▒██▒  ██▒ ██▄█▒  ██  ▓██▒▓██░  ██▒
+      ▒██▒▓██░ ██▓▒   ▒██░    ▒██░  ██▒▒██░  ██▒▓███▄░ ▓██  ▒██░▓██░ ██▓▒
+      ░██░▒██▄█▓▒ ▒   ▒██░    ▒██   ██░▒██   ██░▓██ █▄ ▓▓█  ░██░▒██▄█▓▒ ▒
+      ░██░▒██▒ ░  ░   ░██████▒░ ████▓▒░░ ████▓▒░▒██▒ █▄▒▒█████▓ ▒██▒ ░  ░
+      ░▓  ▒▓▒░ ░  ░   ░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░▒░▒░ ▒ ▒▒ ▓▒░▒▓▒ ▒ ▒ ▒▓▒░ ░  ░
+      ▒ ░░▒ ░        ░ ░ ▒  ░  ░ ▒ ▒░   ░ ▒ ▒░ ░ ░▒ ▒░░░▒░ ░ ░ ░▒ ░
+      ▒ ░░░            ░ ░   ░ ░ ░ ▒  ░ ░ ░ ▒  ░ ░░ ░  ░░░ ░ ░ ░░
+      ░                  ░  ░    ░ ░      ░ ░  ░  ░      ░
+"""
+menu2 = """
+[0] Back to main
+[1] IP Info
+[2] IP Ping
+[3] Port Scan
+[4] Reverse DNS
+"""
+
 def ip_info(ip_address):
     response = requests.get(f'http://ip-api.com/json/{ip_address}')
     
@@ -36,11 +55,11 @@ def ip_info(ip_address):
             }
             
             for key, value in result.items():
-                print(f"{key} : {value}")
+                print(f"\033[31m{key} : {value}\033[0m")
         else:
-            print("Veuillez vérifier l'adresse IP et réessayer.")
+            print("\033[31mVeuillez vérifier l'adresse IP et réessayer.\033[0m")
     else:
-        print("Veuillez réessayer plus tard **(API)**.")
+        print("\033[31mVeuillez réessayer plus tard **(API)**.\033[0m")
         
 def ping_ip(ip_address):
     param = '-n' if platform.system().lower() == 'windows' else '-c'
@@ -48,12 +67,12 @@ def ping_ip(ip_address):
     try:
         result = subprocess.run(['ping', param, '4', ip_address], capture_output=True, text=True, timeout=10)
         print(f"\033[31m\n{'=' * 60}\nPINGING {ip_address}\n{'=' * 60}\033[0m")
-        print(result.stdout)
+        print(f"\033[31m{result.stdout}\033[0m")
         
     except subprocess.TimeoutExpired:
-        print("Le ping a expiré (Timeout).")
+        print("\033[31mLe ping a expiré (Timeout).\033[0m")
     except Exception as e:
-        print(f"Une erreur est survenue : {e}")
+        print(f"\033[31mUne erreur est survenue : {e}\033[0m")
         
 def scan_port(ip_address, port):
     try:
@@ -67,52 +86,33 @@ def scan_port(ip_address, port):
         
 def port_scan(ip_address):
     open_ports = []
-    print(f"Scanning ports on {ip_address}... This may take a while.")
+    print(f"\033[31mScanning ports on {ip_address}... This may take a while.\033[0m")
     with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
         futures = {executor.submit(scan_port, ip_address, port): port for port in range(1, 1025)}
         for future in concurrent.futures.as_completed(futures):
             port = futures[future]
             if future.result():
                 open_ports.append(port)
-                print(f"Port {port} is open")
+                print(f"\033[31mPort {port} is open\033[0m")
 
     if open_ports:
-        print(f"\n{'=' * 60}\nOPEN PORTS ON {ip_address}\n{'=' * 60}")
+        print(f"\n\033[31m{'=' * 60}\nOPEN PORTS ON {ip_address}\n{'=' * 60}\033[0m")
         print(f"\033[31mPorts Ouverts: {open_ports}\033[0m")
     else:
-        print(f"Aucun port ouvert trouvé sur {ip_address}.")
+        print(f"\033[31mAucun port ouvert trouvé sur {ip_address}.\033[0m")
 
 def reverse_dns(ip_address):
     try:
         result = subprocess.run(['nslookup', ip_address], capture_output=True, text=True)
         print(f"\033[31m\n{'=' * 60}\nREVERSE DNS LOOKUP {ip_address}\n{'=' * 60}\033[0m")
-        print(result.stdout)
+        print(f"\033[31m{result.stdout}\033[0m")
     except FileNotFoundError:
-        print("La commande 'nslookup' n'a pas été trouvée.")
+        print("\033[31mLa commande 'nslookup' n'a pas été trouvée.\033[0m")
     except Exception as e:
-        print(f"Une erreur est survenue : {e}")
+        print(f"\033[31mUne erreur est survenue : {e}\033[0m")
 
 def show_menu():
-    menu = """
-          ██▓ ██▓███      ██▓     ▒█████   ▒█████   ██ ▄█▀ █    ██  ██▓███
-          ▓██▒▓██░  ██▒   ▓██▒    ▒██▒  ██▒▒██▒  ██▒ ██▄█▒  ██  ▓██▒▓██░  ██▒
-          ▒██▒▓██░ ██▓▒   ▒██░    ▒██░  ██▒▒██░  ██▒▓███▄░ ▓██  ▒██░▓██░ ██▓▒
-          ░██░▒██▄█▓▒ ▒   ▒██░    ▒██   ██░▒██   ██░▓██ █▄ ▓▓█  ░██░▒██▄█▓▒ ▒
-          ░██░▒██▒ ░  ░   ░██████▒░ ████▓▒░░ ████▓▒░▒██▒ █▄▒▒█████▓ ▒██▒ ░  ░
-          ░▓  ▒▓▒░ ░  ░   ░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░▒░▒░ ▒ ▒▒ ▓▒░▒▓▒ ▒ ▒ ▒▓▒░ ░  ░
-          ▒ ░░▒ ░        ░ ░ ▒  ░  ░ ▒ ▒░   ░ ▒ ▒░ ░ ░▒ ▒░░░▒░ ░ ░ ░▒ ░
-          ▒ ░░░            ░ ░   ░ ░ ░ ▒  ░ ░ ░ ▒  ░ ░░ ░  ░░░ ░ ░ ░░
-          ░                  ░  ░    ░ ░      ░ ░  ░  ░      ░
-"""
-    menu2 = """
-[0] Back to main
-[1] IP Info
-[2] IP Ping
-[3] Port Scan
-[4] Reverse DNS
-"""
-
-    print(f"\033[31m{menu}")
+    print(f"\033[31m{menu}\033[0m")
     print(f"\033[31m{menu2}\033[0m")
 
 def main():
@@ -120,27 +120,35 @@ def main():
         os.system('cls' if os.name == 'nt' else 'clear')
         show_menu()
         try:
-            choice = int(input('Choice >> '))
+            choice = int(input("\033[31mChoice >> \033[0m"))
             if choice == 0:
                 os.system('python cyb3rtech.py')
                 break
             elif choice == 1:
-                ip_address = input("Adresse IP >> ")
+                ip_address = input("\033[31mAdresse IP >> \033[0m")
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(f"\033[31m{menu}\033[0m")
                 ip_info(ip_address)
             elif choice == 2:
-                ip_address = input("Adresse IP >> ")
+                ip_address = input("\033[31mAdresse IP >> \033[0m")
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(f"\033[31m{menu}\033[0m")
                 ping_ip(ip_address)
             elif choice == 3:
-                ip_address = input("Adresse IP >> ")
+                ip_address = input("\033[31mAdresse IP >> \033[0m")
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(f"\033[31m{menu}\033[0m")
                 port_scan(ip_address)
             elif choice == 4:
-                ip_address = input("Adresse IP >> ")
+                ip_address = input("\033[31mAdresse IP >> \033[0m")
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(f"\033[31m{menu}\033[0m")
                 reverse_dns(ip_address)
             else:
-                print("\033[31m[!] >\033[0m Invalid choice \033[31m< [!]\033[0m")
+                print("\033[31m[!]\033[0m Invalid choice \033[31m[!]\033[0m")
         except ValueError:
-            print("Veuillez entrer un nombre valide.")
-        input("\nAppuyez sur Entrée pour retourner au menu...\033[0m")
+            print("\033[31mPlease enter a valid number\033[0m")
+        input("\n\033[31mPress Enter to return to the menu...\033[0m")
 
 if __name__ == "__main__":
-    main()    
+    main()
