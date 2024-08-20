2113bdc42dc65a78bb1f06417d7ad1065fefee5a
Cyb3rtch <174739196+Cyb3rtch@users.noreply.github.com>
2024-08-20 22:25:02 +0200

    Update discord.py

--- tools/discord.py
+++ tools/discord.py
@@ -1,8 +1,8 @@
 import os
 import webbrowser
+import time
 
-def show_menu():
-    menu = """
+menu = """
              ▓█████▄  ██▓  ██████  ▄████▄   ▒█████   ██▀███  ▓█████▄
              ▒██▀ ██▌▓██▒▒██    ▒ ▒██▀ ▀█  ▒██▒  ██▒▓██ ▒ ██▒▒██▀ ██▌
              ░██   █▌▒██▒░ ▓██▄   ▒▓█    ▄ ▒██░  ██▒▓██ ░▄█ ▒░██   █▌
@@ -14,10 +14,19 @@ def show_menu():
              ░     ░        ░  ░ ░          ░ ░     ░        ░
              ░                   ░
 """
+menu2 = """
+[0] Back to main
+"""
+def show_menu
     print(f"\033[31m{menu}")
+    print(f"{menu2}\033[0m")
+   
+time.sleep(1) 
+webbrowser.open('https://discord.gg/mP6NvAgF2q')
 
 def main():
     while True:
+    os.system('cls' if os.name == 'nt' else 'clear')
         show_menu()
         try:
             choice = int(input('Choose >> '))
@@ -27,7 +36,7 @@ def main():
                 print("\033[31m[!] >\033[0m Invalid choice \033[31m< [!]\033[0m")
         except ValueError:
             print("Veuillez entrer un nombre valide.")
-        input("\nAppuyez sur Entrée pour retourner au menu..\033[0m")
+        input("\nPress Enter to return to the main menu...\033[0m")
 
 if __name__ == "__main__":
     main()
