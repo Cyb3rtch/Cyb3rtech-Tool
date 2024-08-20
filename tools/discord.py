c42d507568eb012a921f34af9224e404e172b90b
Cyb3rtch <174739196+Cyb3rtch@users.noreply.github.com>
2024-08-20 23:12:35 +0200

    Update discord.py

--- tools/discord.py
+++ tools/discord.py
@@ -17,25 +17,27 @@ menu = """
 menu2 = """
 [0] Back to main
 """
+
 def show_menu():
     print(f"\033[31m{menu}")
     print(f"{menu2}\033[0m")
-   
-time.sleep(1) 
+
+time.sleep(1)
 webbrowser.open('https://discord.gg/mP6NvAgF2q')
 
 def main():
     while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
+        os.system('cls' if os.name == 'nt' else 'clear')
         show_menu()
         try:
             choice = int(input('Choose >> '))
-            if choice == 1:
-                os.system('python cyb3rtech.py')
+            if choice == 0:
+              os.system('python cyb3rtech.py')
+                break
             else:
                 print("\033[31m[!] >\033[0m Invalid choice \033[31m< [!]\033[0m")
         except ValueError:
-            print("Veuillez entrer un nombre valide.")
+            print("\033[31m[!] Please enter a valid number\033[0m")
         input("\nPress Enter to return to the main menu...\033[0m")
 
 if __name__ == "__main__":
