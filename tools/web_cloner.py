f164032a7caa83ccf79d4d8ca4057e7fb934b69b
Cyb3rtch <174739196+Cyb3rtch@users.noreply.github.com>
2024-08-23 18:57:37 +0200

    Update web_cloner.py

--- tools/web_cloner.py
+++ tools/web_cloner.py
@@ -1,5 +1,6 @@
 from pywebcopy import save_website
 import os
+import time
 
 menu = """
          █     █░▓█████  ▄▄▄▄       ▄████▄   ██▓     ▒█████   ███▄    █ ▓█████  ██▀███
@@ -22,13 +23,10 @@ def show_menu():
     print(f"\033[31m{menu}\033[0m")
     print(f"\033[31m{menu2}\033[0m")
 
-def clone_website(url, folder):
-    save_website(
-        url=url,
-        project_folder=folder,
-        open_in_browser=False
-    )
-    print(f"\033[31mWebsite cloner dans le dossier : {folder}\033[0m")
+def clone():
+    print("\033[31mEn cours de création.. (redirection)\033[0m")
+    time.sleep(2)
+    os.system('python cyb3rtech.py')
 
 def main():
     while True:
@@ -37,13 +35,12 @@ def main():
         try:
             choice = int(input('\033[31mChoice >> \033[0m'))
             if choice == 0:
+                os.system('python cyb3rtech.py')
                 break
             elif choice == 1:
-                url = input('\033[31mWebsite Url (e.g., http://exemple.com): \033[0m').strip()
-                folder = 'web-cloner'
                 os.system('cls' if os.name == 'nt' else 'clear')
-                clone_website(url, folder)
-                input("\n\033[31mPress Enter to return to the menu...\033[0m")
+                print(f"\033[31m{menu}\033[0m")
+                clone()
             else:
                 print("\033[31m[!]\033[0m Invalid choice \033[31m[!]\033[0m")
                 input("\n\033[31mPress Enter to return to the menu...\033[0m")
