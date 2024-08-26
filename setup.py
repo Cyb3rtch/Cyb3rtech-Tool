409fc165c703f254693f5bec9fc1c6c1b678f74c
Cyb3rtch <174739196+Cyb3rtch@users.noreply.github.com>
2024-08-27 00:01:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 import sys
 import os
 
-    print("Installing the python modules required 
-    ..")
+print("Installing the python modules required...")
 
+try:
     if sys.platform.startswith("win"):
         os.system("python -m pip install --upgrade pip")
         os.system("python -m pip install -r requirements.txt")
         os.system("python cyb3rtech.py")
 
-    elif sys.platform.startswith("linux"):
-        os.system("python3 -m pip3 install --upgrade pip")
-        os.system("python3 -m pip3 install -r requirements.txt")
+    elif sys.platform.startswith("linux") or sys.platform.startswith("darwin"):  # darwin is the platform name for macOS
+        os.system("python3 -m pip install --upgrade pip")
+        os.system("python3 -m pip install -r requirements.txt")
         os.system("python3 cyb3rtech.py")
 
+    else:
+        print("Unsupported platform:", sys.platform)
+
 except Exception as e:
-    print(e)
-    os.system("pause")
+    print("An error occurred:", e)
+    if sys.platform.startswith("win"):
+        os.system("pause")
